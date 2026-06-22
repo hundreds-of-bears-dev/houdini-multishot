b9cea7b25f957ed905914b08b82b7e74887df994
hundredsofbears <hundredsofbears@proton.me>
2026-06-21 20:37:27 -0700

    pared down placeholder

---
+++ package.py
@@ -0,0 +1,13 @@
+name = "houdini_multishot"
+
+version = "0.0.1"
+
+authors = ["hundredsofbears",]
+
+requires = ["houdini"]
+
+def commands():
+    print("\nwe've done it\n")
+
+uuid = "houdini_multishot"
+
