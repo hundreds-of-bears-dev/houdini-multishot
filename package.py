8136a6c7c435cf80713cf19839311e80eb015ae8
hundredsofbears <hundredsofbears@proton.me>
2026-06-24 08:26:00 -0700

    added otls dir

--- package.py
+++ package.py
@@ -11,6 +11,9 @@ def commands():
     if "HOUDINI_DSO_PATH" not in env.keys():
         env.HOUDINI_DSO_PATH.set("&")
     env.HOUDINI_DSO_PATH.prepend("{root}/dso")
+    if "HOUDINI_OTLSCAN_PATH" not in env.keys():
+        env.HOUDINI_OTLSCAN_PATH.set("&")
+    env.HOUDINI_OTLSCAN_PATH.prepend("{root}/otls")
 
 uuid = "houdini_multishot"
 
