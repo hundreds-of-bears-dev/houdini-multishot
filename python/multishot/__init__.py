196a6ebc7aed88262225f31f583c6964cf490d38
hundredsofbears <hundredsofbears@proton.me>
2026-06-28 10:55:14 -0700

    added pdg support to expand_str

--- python/multishot/__init__.py
+++ python/multishot/__init__.py
@@ -1,13 +1,25 @@
 
 import hou
+import pdg
 import string
 
 
+def eval_pdg(attrname):
+    if not (active := pdg.workItem()):
+        return None
+    if not (attr := active.attrib(attrname)):
+        return None
+    return attr.value()
+
+
+# TODO: add support for passing in an index to check
 def expand_str(val: str, encode:bool=False):
     result = []
-    formatter = string.Formatter()
-    for head, field, spec, conversion in formatter.parse(val):
+    for head, field, spec, conversion in string.Formatter().parse(val):
         result.append(head)
-        result.append(hou.contextOption(field) or '')
+        if field:
+            pdgval = eval_pdg(field)
+            pdgvals = str(pdgval) if pdgval else None
+            result.append(pdgvals or hou.contextOption(field) or '')
     return ''.join(result)
 
